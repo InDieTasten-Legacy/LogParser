dfc8b208aa4b70190dbaedcf01b32e07918de5a6
bubbley <werremeyerjan@yahoo.de>
2014-09-20 22:27:56 +0200

    Release

--- build-UntiteldGame_LogParser-Desktop_Qt_5_3_MSVC2013_OpenGL_32bit-Release/ui_mainwindow.h
+++ build-UntiteldGame_LogParser-Desktop_Qt_5_3_MSVC2013_OpenGL_32bit-Release/ui_mainwindow.h
@@ -14,9 +14,7 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
-#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
@@ -32,11 +30,8 @@ public:
     QAction *actionExit;
     QAction *actionHTML;
     QWidget *centralWidget;
-    QLineEdit *lineEdit;
     QPushButton *pushButton;
     QMenuBar *menuBar;
-    QMenu *menuFile;
-    QMenu *menuExport;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
 
@@ -44,7 +39,8 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(1028, 557);
+        MainWindow->resize(116, 86);
+        MainWindow->setMaximumSize(QSize(116, 86));
         actionReload = new QAction(MainWindow);
         actionReload->setObjectName(QStringLiteral("actionReload"));
         actionExit = new QAction(MainWindow);
@@ -53,20 +49,13 @@ public:
         actionHTML->setObjectName(QStringLiteral("actionHTML"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        lineEdit = new QLineEdit(centralWidget);
-        lineEdit->setObjectName(QStringLiteral("lineEdit"));
-        lineEdit->setGeometry(QRect(10, 10, 941, 21));
         pushButton = new QPushButton(centralWidget);
         pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(950, 10, 75, 23));
+        pushButton->setGeometry(QRect(20, 10, 75, 23));
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 1028, 20));
-        menuFile = new QMenu(menuBar);
-        menuFile->setObjectName(QStringLiteral("menuFile"));
-        menuExport = new QMenu(menuBar);
-        menuExport->setObjectName(QStringLiteral("menuExport"));
+        menuBar->setGeometry(QRect(0, 0, 116, 20));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -74,12 +63,6 @@ public:
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindow->setStatusBar(statusBar);
-
-        menuBar->addAction(menuFile->menuAction());
-        menuBar->addAction(menuExport->menuAction());
-        menuFile->addAction(actionReload);
-        menuFile->addAction(actionExit);
-        menuExport->addAction(actionHTML);
 
         retranslateUi(MainWindow);
 
@@ -92,9 +75,7 @@ public:
         actionReload->setText(QApplication::translate("MainWindow", "Reload", 0));
         actionExit->setText(QApplication::translate("MainWindow", "Exit", 0));
         actionHTML->setText(QApplication::translate("MainWindow", "HTML", 0));
-        pushButton->setText(QApplication::translate("MainWindow", "PushButton", 0));
-        menuFile->setTitle(QApplication::translate("MainWindow", "File", 0));
-        menuExport->setTitle(QApplication::translate("MainWindow", "Export", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "parse", 0));
     } // retranslateUi
 
 };
